4878059b06a2063970d9fa49e0c0f7a51f46ed87
instant.sc <code@insc.cc>
2025-03-04 23:41:17 +0100

    Offload initial load to the thread pool

--- PluginUpdater.cs
+++ PluginUpdater.cs
@@ -1,21 +1,33 @@
-﻿using ExileCore;
+﻿using System.Threading.Tasks;
+using ExileCore;
 
 namespace PluginUpdater;
 
 public class PluginUpdater : BaseSettingsPlugin<PluginUpdaterSettings>
 {
     public static PluginUpdater Instance;
+    private Task _startupTask;
 
     public override bool Initialise()
     {
         Instance = this;
         Settings.GameController = GameController;
-        Settings.PluginConfig.Startup();
+        _startupTask = Task.Run(Settings.PluginConfig.Startup);
         return true;
     }
 
     public override void Render()
     {
+        if (_startupTask == null || !_startupTask.IsCompletedSuccessfully)
+        {
+            if (_startupTask?.IsFaulted == true)
+            {
+                DebugWindow.LogError(_startupTask.Exception.ToString());
+            }
+
+            return;
+        }
+
         Settings.PluginConfig.Update();
     }
 }
