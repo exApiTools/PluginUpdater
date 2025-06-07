b81d8e266940e9d1a7b15d19bf943a655656cae9
instant.sc <code@insc.cc>
2025-06-08 01:45:23 +0200

    Mark non-menu properties as ignored

--- PluginUpdaterSettings.cs
+++ PluginUpdaterSettings.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using ExileCore;
+using ExileCore.Shared.Attributes;
 using ExileCore.Shared.Interfaces;
 using ExileCore.Shared.Nodes;
 using Newtonsoft.Json;
@@ -14,18 +15,30 @@ public class PluginUpdaterSettings : ISettings
     }
 
     public ToggleNode Enable { get; set; } = new ToggleNode(true);
+
+    [IgnoreMenu]
     public bool CheckUpdatesOnStartup { get; set; }
+
+    [IgnoreMenu]
     public bool AutoCheckUpdates { get; set; }
+
+    [IgnoreMenu]
     public bool WrapLogMessages { get; set; } = true;
 
+    [IgnoreMenu]
     public int UpdateCheckIntervalMinutes { get; set; } = 60;
 
     [JsonIgnore]
+    [IgnoreMenu]
     public DateTime LastUpdateCheck { get; set; } = DateTime.Now;
+
     [JsonIgnore]
+    [IgnoreMenu]
     public bool HasCheckedUpdates { get; set; } = false;
+
     [JsonIgnore]
     public PluginRenderer PluginConfig { get; set; }
+
     [JsonIgnore]
     public GameController GameController { get; set; } // this is very lazy
 
